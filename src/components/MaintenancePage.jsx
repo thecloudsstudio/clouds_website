7d36f0a9928e0dcba6f3cbb988b79d13137d0275
Your Name <your.email@example.com>
2025-11-24 13:45:27 +0530

    Restore cursor with smaller size

--- src/components/MaintenancePage.jsx
+++ src/components/MaintenancePage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Cursor from './ui/inverted-cursor';
 
 
 const MaintenancePage = () => {
     return (
         <div className="relative w-full h-screen overflow-hidden bg-white text-black flex flex-col items-center justify-center font-sans cursor-none">
+            <Cursor size={20} />
 
 
             <div className="z-10 flex flex-col items-center justify-center space-y-8 text-center px-4">
